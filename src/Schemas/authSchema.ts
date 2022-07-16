9aea5ab81eacb916e827797bb2942d6f9d5b4988
Rafael Longo <rafaelongo45@gmail.com>
2022-07-16 11:08:19 -0300

    Fix: Requires signup and signin to send a password confirmation

--- src/Schemas/authSchema.ts
+++ src/Schemas/authSchema.ts
@@ -2,7 +2,12 @@ import joi from "joi";
 
 import { Users } from "@prisma/client";
 
-export const authSchema = joi.object<Users>({
+interface UserData extends Users {
+  confirmPassword: string
+}
+
+export const authSchema = joi.object<UserData>({
   email: joi.string().email().required(),
-  password: joi.string().min(10).required()
+  password: joi.string().min(10).required(),
+  confirmPassword: joi.ref('password')
 })
